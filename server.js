d093e3cac89eac349314983079e65b12b408ce21
Tony Bradley <antbradley91@gmail.com>
2017-02-17 20:09:15 -0500

    add babel so uglifyjs works

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const http = require('http')
 const path = require('path')
 const express = require('express')
 const app = express()
@@ -7,20 +7,10 @@ app.use(express.static(
   path.join(__dirname, './public')
 ))
 
-app.get(/^\/view.+$/, serveIndex)
+const server = http.createServer(function (req, res) {
+  app(req, res)
+})
 
-let indexPage
-
-function serveIndex (req, res) {
-  if (indexPage) return res.send(indexPage)
-  fs.readFile(
-    path.join(__dirname, './public/index.html'),
-    'utf8',
-    function (err, data) {
-      if (err) throw err
-      res.send(data)
-    }
-  )
-}
-
-app.listen(3000)
+server.listen(3000, function () {
+  console.log('server listening on port 3000')
+})
